b8788028bf3b5c3703c5a69aadd341fbd7082f02
fstorresneto@gmail.com <fstorresneto@gmail.com>
2021-07-02 15:29:15 -0300

    ADAPTAÇÃO DE SITE

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -38,5 +38,10 @@ namespace SalesWebMvc.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult StartLogin()
+        {
+            return View();
+        }
     }
 }
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace SalesWebMvc
 {
@@ -17,10 +12,11 @@ namespace SalesWebMvc
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+                 Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-    }
+
+  }
 }
